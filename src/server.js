be11807c4e1a0f338c62703727f61fb7b5649e16
Chamngary Phayrin <cham.phayrin@gmail.com>
2025-01-11 20:47:21 -0500

    server change

--- src/server.js
+++ src/server.js
@@ -33,140 +33,76 @@ app.use(express.json());
 
 // Home route
 app.get("/", (req, res) => {
-<<<<<<< HEAD
 	res.render("index", {
 		// Render the 'index' view
 		title: "Muse",
 		description:
 			"Welcome to Muse, where we challenge the growing influence of AI in the art world. Explore the rich history of human-created art and learn how AI is erasing traditional craftsmanship, overshadowing human creativity, and threatening the preservation of our artistic heritage.",
 	});
-=======
-  res.render("index", {
-    // Render the 'index' view
-    title: "Muse",
-    description:
-      "Welcome to Muse, where we challenge the growing influence of AI in the art world. Explore the rich history of human-created art and learn how AI is erasing traditional craftsmanship, overshadowing human creativity, and threatening the preservation of our artistic heritage.",
-  });
->>>>>>> 0e4daf4fa64c9317037a6436d678f96571171df2
 });
 
 // About Us route
 app.get("/aboutus", (req, res) => {
-<<<<<<< HEAD
 	res.render("aboutus", {
 		// Render the 'aboutUs' view
 		title: "About Us",
 		description:
 			"Muse is committed to raising awareness about how AI is threatening the authenticity and legacy of human-made art. We believe that AI-driven art not only erases the value of history but also undermines the very essence of creative expression by displacing traditional artists.",
 	});
-=======
-  res.render("aboutus", {
-    // Render the 'aboutUs' view
-    title: "About Us",
-    description:
-      "Muse is committed to raising awareness about how AI is threatening the authenticity and legacy of human-made art. We believe that AI-driven art not only erases the value of history but also undermines the very essence of creative expression by displacing traditional artists.",
-  });
->>>>>>> 0e4daf4fa64c9317037a6436d678f96571171df2
 });
 
 // Login route
 app.get("/login", (req, res) => {
-<<<<<<< HEAD
 	res.render("login", {
 		// Render the 'login' view
 		title: "Login",
 		description:
 			"Log in to ArtFuture to join the conversation on how AI is undermining the legacy of human-made art. If you're new, sign up to start learning about the dangers of AI-driven art and why it’s crucial to preserve our artistic history.",
 	});
-=======
-  res.render("login", {
-    // Render the 'login' view
-    title: "Login",
-    description:
-      "Log in to ArtFuture to join the conversation on how AI is undermining the legacy of human-made art. If you're new, sign up to start learning about the dangers of AI-driven art and why it’s crucial to preserve our artistic history.",
-  });
->>>>>>> 0e4daf4fa64c9317037a6436d678f96571171df2
 });
 
 // Sign-Up route
 app.get("/signup", (req, res) => {
-<<<<<<< HEAD
 	res.render("signup", {
 		// Render the 'signUp' view
 		title: "Sign up",
 		description:
 			"Sign up to become part of Muse's community that’s fighting to protect human creativity from the rise of AI in art. Gain access to discussions, resources, and tools dedicated to preserving the integrity of traditional art forms and the artists who created them.",
 	});
-=======
-  res.render("signup", {
-    // Render the 'signUp' view
-    title: "Sign up",
-    description:
-      "Sign up to become part of Muse's community that’s fighting to protect human creativity from the rise of AI in art. Gain access to discussions, resources, and tools dedicated to preserving the integrity of traditional art forms and the artists who created them.",
-  });
->>>>>>> 0e4daf4fa64c9317037a6436d678f96571171df2
 });
 
 // Action route
 app.get("/resources", (req, res) => {
-<<<<<<< HEAD
 	res.render("resources", {
 		// Render the 'resources' view
 		title: "Resources",
 		description:
 			"Our resource library explores the evolving role of AI in the art world, examining both its potential and its challenges. We provide valuable insights into how AI is influencing traditional artistry and the preservation of historical techniques, fostering a thoughtful conversation about the future of art.",
 	});
-=======
-  res.render("resources", {
-    // Render the 'resources' view
-    title: "Resources",
-    description:
-      "Our resource library dives deep into the harmful impact AI is having on the art world. From the displacement of traditional artists to the erasure of historical techniques, we provide critical information to raise awareness about the threats to art history posed by AI.",
-  });
->>>>>>> 0e4daf4fa64c9317037a6436d678f96571171df2
 });
 
 // Search route
 app.get("/explore", (req, res) => {
-<<<<<<< HEAD
 	res.render("explore", {
 		// Render the 'explore' view
 		title: "Explore",
 		description:
 			"Browse Search for artworks from the Art Institute of Chicago’s collection and view key details like title, artist, and description. Descriptions are cleaned for clarity, and each artwork is displayed with a high-quality image. Explore the richness of human-made art while considering how AI is transforming the creative landscape. art that represents the legacy of human creativity and the ongoing battle against AI’s intrusion into the art world. Use our search tools to uncover how AI-generated art lacks the depth and humanity of traditional masterpieces, and see why preserving human-created art is more urgent than ever.",
 	});
-=======
-  res.render("explore", {
-    // Render the 'explore' view
-    title: "Explore",
-    description:
-      "Browse Search for artworks from the Art Institute of Chicago’s collection and view key details like title, artist, and description. Descriptions are cleaned for clarity, and each artwork is displayed with a high-quality image. Explore the richness of human-made art while considering how AI is transforming the creative landscape. art that represents the legacy of human creativity and the ongoing battle against AI’s intrusion into the art world. Use our search tools to uncover how AI-generated art lacks the depth and humanity of traditional masterpieces, and see why preserving human-created art is more urgent than ever.",
-  });
->>>>>>> 0e4daf4fa64c9317037a6436d678f96571171df2
 });
 
 // Contact route
 app.get("/contactus", (req, res) => {
-<<<<<<< HEAD
 	res.render("contactus", {
 		// Render the 'explore' view
 		title: "Contact Us",
 		description:
 			"Have questions about the Art Institute of Chicago’s collection? Reach out to us for details on artworks, artists, and how we’re preserving human creativity in an age of AI-driven art.",
 	});
-=======
-  res.render("contactus", {
-    // Render the 'explore' view
-    title: "Contact Us",
-    description:
-      "Browse Search for artworks from the Art Institute of Chicago’s collection and view key details like title, artist, and description. Descriptions are cleaned for clarity, and each artwork is displayed with a high-quality image. Explore the richness of human-made art while considering how AI is transforming the creative landscape. art that represents the legacy of human creativity and the ongoing battle against AI’s intrusion into the art world. Use our search tools to uncover how AI-generated art lacks the depth and humanity of traditional masterpieces, and see why preserving human-created art is more urgent than ever.",
-  });
->>>>>>> 0e4daf4fa64c9317037a6436d678f96571171df2
 });
 
 // Query route
 app.get("/q", async (req, res) => {
-<<<<<<< HEAD
 	const q = req.query.q;
 
 	if (!q) {
@@ -202,48 +138,10 @@ app.post("/register", (req, res) => {
 	if (password != repeatPassword) {
 		return res.send({ error: "Password do not match!" });
 	}
-=======
-  const q = req.query.q;
-
-  if (!q) {
-    return res.send("Error: Search query is required");
-  }
-
-  try {
-    // Fetch artworks
-    const artworks = await getArtwork(q);
-
-    // Get the number of artworks to fetch that many historical events
-    const events = await getHistoricalEvents(q, artworks.length);
-
-    // Return both the artworks and historical events
-    return res.json({
-      artworks,
-      historicalEvents: events,
-    });
-  } catch (error) {
-    console.error("Error:", error);
-    return res.status(500).send("Error fetching data");
-  }
-});
-
-app.post("/register", (req, res) => {
-  const { firstname, lastname, email, password, repeatPassword } = req.body;
-
-  if (password != repeatPassword) {
-    return res.send({ error: "Password do not match!" });
-  }
-
-  const insertQ =
-    "INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?)";
-
-  const selectEmailQ = `SELECT email FROM users WHERE email = '${email}'`;
->>>>>>> 0e4daf4fa64c9317037a6436d678f96571171df2
 
 	const insertQ =
 		"INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?)";
 
-<<<<<<< HEAD
 	const selectEmailQ = `SELECT email FROM users WHERE email = '${email}'`;
 
 	const values = [firstname, lastname, email, password];
@@ -320,30 +218,4 @@ app.get("*", (req, res) => {
 // CHECK SERVER IS UP
 app.listen(port, () => {
 	console.log(`Server is up on port ${port}!`);
-=======
-  connection.query(selectEmailQ, (err, result) => {
-    if (err) throw err;
-    if (result.length > 0) {
-      return res.send({ error: "Email in use!" });
-    } else {
-      connection.query(insertQ, values, (err, result) => {
-        if (err) throw err;
-        res.redirect("/login");
-      });
-    }
-  });
-});
-
-// Non-existing routes
-app.get("*", (req, res) => {
-  res.render("404", {
-    // Render the '404' view
-    title: "Page Not Found",
-  });
-});
-
-// CHECK SERVER IS UP
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}!`);
->>>>>>> 0e4daf4fa64c9317037a6436d678f96571171df2
 });
